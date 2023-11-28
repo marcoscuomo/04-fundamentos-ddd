34e3c01f713713ce74dc02b5c4c7a872404b60f6
Marcos Souza <marcos.souza@beeleads.com.br>
2023-11-28 18:23:35 -0300

    feat: valie object slug

---
+++ src/domain/entities/value-objects/slug.ts
@@ -0,0 +1,29 @@
+export class Slug {
+  public value: string
+
+  constructor(value: string) {
+    this.value = value
+  }
+
+
+  /**
+   * Receives a strung and normalize it as a slig.
+   * 
+   * Example: "An example title" => "an-example-title
+   * 
+   * @param text {string}"
+  */
+  static createFromText(text: string) {
+    const slugText = text
+    .normalize("NFKC")
+    .toLocaleLowerCase()
+    .trim()
+    .replace(/\s+/g, '-') //remove all white spaces
+    .replace(/[^\w-]+/g, '') //remove all no words
+    .replace(/_/g, '-') 
+    .replace(/--+/g, '-')
+    .replace(/-$/g, '')
+
+    return new Slug(slugText)
+  }
+}
