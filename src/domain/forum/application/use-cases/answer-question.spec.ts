467f50a4e39c5c69c955b76ad4ea76e5a670d19d
Marcos Souza <marcos.souza@beeleads.com.br>
2023-11-30 11:48:39 -0300

    feat: Refatorando testes unitarios

---
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -0,0 +1,23 @@
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { AnswerQuestionUseCase } from './answer-question'
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let sut: AnswerQuestionUseCase
+
+describe('Create Answer', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    sut = new AnswerQuestionUseCase(inMemoryAnswersRepository)
+  })
+
+  it('should be able to create an answer', async () => {
+    const { answer } = await sut.execute({
+      questionId: '1',
+      instructorId: '1',
+      content: 'Content answer',
+    })
+
+    expect(answer.id).toBeTruthy() // não pode ser nulo, undefined, void
+    expect(inMemoryAnswersRepository.items[0].id).toEqual(answer.id)
+  })
+})
--- src/domain/forum/enterprise/entities/value-objects/slug.ts
+++ src/domain/forum/enterprise/entities/value-objects/slug.ts
@@ -5,24 +5,23 @@ export class Slug {
     this.value = value
   }
 
-
   /**
    * Receives a strung and normalize it as a slig.
-   * 
+   *
    * Example: "An example title" => "an-example-title
-   * 
+   *
    * @param text {string}"
-  */
+   */
   static createFromText(text: string) {
     const slugText = text
-    .normalize("NFKC")
-    .toLocaleLowerCase()
-    .trim()
-    .replace(/\s+/g, '-') //remove all white spaces
-    .replace(/[^\w-]+/g, '') //remove all no words
-    .replace(/_/g, '-') 
-    .replace(/--+/g, '-')
-    .replace(/-$/g, '')
+      .normalize('NFKC')
+      .toLocaleLowerCase()
+      .trim()
+      .replace(/\s+/g, '-') // remove all white spaces
+      .replace(/[^\w-]+/g, '') // remove all no words
+      .replace(/_/g, '-')
+      .replace(/--+/g, '-')
+      .replace(/-$/g, '')
 
     return new Slug(slugText)
   }
