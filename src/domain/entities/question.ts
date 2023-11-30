650d25670d035d17e24449d13ed7e53fcfc51bc5
Marcos Souza <marcos.souza@beeleads.com.br>
2023-11-30 08:25:37 -0300

    feat: path aliases

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,25 +1,99 @@
-import { randomUUID } from "node:crypto"
 import { Slug } from "./value-objects/slug"
+import { Entity } from "@/core/entities/entity"
+import { UniqueEntityID } from "@/core/entities/unique-entity-id"
+import { Optional } from "@/core/types/optional"
+import dayjs from "dayjs"
 
 interface QuestionProps {
+	authorId: UniqueEntityID
+	bestAnswerId?: UniqueEntityID
 	title: string
 	slug: Slug
 	content: string
-	authorId: string
+	createdAt: Date
+	updatedAt?: Date
 }
 
-export class Question {
-	public id: string
-	public title: string
-	public slug: Slug
-	public content: string
-	public authorId: string
+export class Question extends Entity<QuestionProps> {
+	// public title: string
+	// public slug: Slug
+	// public content: string
+	// public authorId: string
 
-	constructor(props: QuestionProps, id?: string) {
-		this.title = props.title
-		this.slug = props.slug
-		this.content = props.content
-		this.authorId = props.authorId
-		this.id = id ?? randomUUID() 
+	// constructor(props: QuestionProps, id?: string) {
+	// 	super(props, id)
+	// 	// this.title = props.title
+	// 	// this.slug = props.slug
+	// 	// this.content = props.content
+	// 	// this.authorId = props.authorId
+	// }
+
+  get authorId() {
+		return this.props.authorId	
+	}
+
+  get bestAnswerId() {
+		return this.props.bestAnswerId	
+	}
+
+  get title() {
+		return this.props.title	
+	}
+
+  get slug() {
+		return this.props.slug	
+	}
+
+  get content() {
+		return this.props.content	
+	}
+
+  get createdAt() {
+		return this.props.createdAt	
+	}
+
+  get updatedAt() {
+		return this.props.updatedAt	
+	}
+
+  get isNew() {
+    return dayjs().diff(this.createdAt, 'days') <= 3
+  }
+
+  get excerpt() {
+    return this.content
+    .substring(0, 120)
+    .trimEnd()
+    .concat('...')
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
+  }
+
+  set title(title: string) {
+    this.props.content = title
+    this.props.slug = Slug.createFromText(title)
+    this.touch()
+  }
+
+  set bestAnswerId(bestAnswerId: UniqueEntityID | undefined) {
+    this.props.bestAnswerId = bestAnswerId
+    this.touch()
+  }
+
+	static create(props: Optional<QuestionProps, 'createdAt' | 'slug'>, id?: UniqueEntityID){
+		const question = new Question({
+			...props,
+      slug: props.slug ?? Slug.createFromText(props.title),
+			createdAt: new Date(),
+		}, id)
+
+    return question
 	}
 }
--- src/domain/entities/student.ts
+++ src/domain/entities/student.ts
@@ -1,12 +1,22 @@
-import { randomUUID } from "node:crypto"
+import { Entity } from "@/core/entities/entity"
+import { UniqueEntityID } from "@/core/entities/unique-entity-id"
+import { Optional } from "@/core/types/optional"
 
-export class Student {
-  public id: string
-  public name: string
-  
+interface StudentProps {
+  name: string
+}
 
-  constructor(name: string, id?: string) {
-    this.name = name
-    this.id = id ?? randomUUID()       
-  }
+export class Student extends Entity<StudentProps> {
+  // public name: string
+
+  // constructor(props: StudentProps, id?: string) {
+  //   super(props, id)
+  //   // this.name = props.name       
+  // }
+
+  static create(props: StudentProps, id?: UniqueEntityID){
+		const student = new Student(props, id)
+
+    return student
+	}
 }
