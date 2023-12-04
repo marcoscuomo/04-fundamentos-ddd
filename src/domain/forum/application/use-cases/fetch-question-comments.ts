86f9b3b8c7164e9a19e5daf908469fab5d198d73
Marcos Souza <marcos.souza@beeleads.com.br>
2023-12-04 16:48:15 -0300

    feat: listar comentários da pergunta

---
+++ src/domain/forum/application/use-cases/fetch-question-comments.ts
@@ -0,0 +1,29 @@
+import { QuestionComment } from '../../enterprise/entities/question-comment'
+import { QuestionCommentRepository } from '../repositories/question-comments-repository'
+
+interface FetchQuestionCommentsUseCaseRequest {
+  questionId: string
+  page: number
+}
+
+interface FetchQuestionCommentsResponse {
+  questionComments: QuestionComment[]
+}
+
+export class FetchQuestionCommentsUseCase {
+  constructor(private questionCommentRepository: QuestionCommentRepository) {}
+
+  async execute({
+    questionId,
+    page,
+  }: FetchQuestionCommentsUseCaseRequest): Promise<FetchQuestionCommentsResponse> {
+    const questionComments =
+      await this.questionCommentRepository.findManyByQuestionId(questionId, {
+        page,
+      })
+
+    return {
+      questionComments,
+    }
+  }
+}
--- src/domain/forum/enterprise/entities/question-comment.ts
+++ src/domain/forum/enterprise/entities/question-comment.ts
@@ -7,7 +7,7 @@ export interface QuestionCommentProps extends CommentProps {
 }
 
 export class QuestionComment extends Comment<QuestionCommentProps> {
-  getquestionId() {
+  get questionId() {
     return this.props.questionId
   }
 
